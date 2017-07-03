680676d8feaf7ca39689455f788365ef784edf9f
maxiaowen <332168460@qq.com>
2017-07-03 18:28:36 +0800

    * 添加联系人图片

--- app/src/main/java/com/atguigu/im0224/controller/fragment/ContactListFragment.java
+++ app/src/main/java/com/atguigu/im0224/controller/fragment/ContactListFragment.java
@@ -1,9 +1,11 @@
 package com.atguigu.im0224.controller.fragment;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import com.atguigu.im0224.R;
+import com.atguigu.im0224.controller.activity.AddContactActivity;
 import com.atguigu.im0224.utils.UIUtils;
 import com.hyphenate.easeui.ui.EaseContactListFragment;
 
@@ -23,6 +25,20 @@ public class ContactListFragment extends EaseContactListFragment {
     protected void setUpView() {
         super.setUpView();
 
+        //初始化Listview头布局
+        initHeadView();
+
+        titleBar.setRightImageResource(R.drawable.ease_blue_add);
+        titleBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),AddContactActivity.class));
+            }
+        });
+    }
+
+
+    private void initHeadView() {
         View headView = View.inflate(getActivity(), R.layout.head_view,null);
 
         final LinearLayout friends = (LinearLayout) headView.findViewById(R.id.ll_new_friends);

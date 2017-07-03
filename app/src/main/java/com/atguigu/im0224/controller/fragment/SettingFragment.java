9f77ee5e76df9d364fabd1b4c7c92518c8116aef
maxiaowen <332168460@qq.com>
2017-07-03 16:07:35 +0800

    设置页面，退出登录功能实现

--- app/src/main/java/com/atguigu/im0224/controller/fragment/SettingFragment.java
+++ app/src/main/java/com/atguigu/im0224/controller/fragment/SettingFragment.java
@@ -1,14 +1,22 @@
 package com.atguigu.im0224.controller.fragment;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+
+import com.atguigu.im0224.R;
+import com.atguigu.im0224.controller.activity.LoginActivity;
+import com.hyphenate.EMCallBack;
+import com.hyphenate.chat.EMClient;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 /**
  * Created by Administrator on 2017/7/3.
@@ -16,17 +24,57 @@ import android.widget.TextView;
 
 public class SettingFragment extends Fragment {
 
+    @Bind(R.id.setting_btn_exit)
+    Button settingBtnExit;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        TextView textView = new TextView(getActivity());
+        View view = View.inflate(getActivity(), R.layout.fragment_settings, null);
 
-        textView.setText("设置");
-        textView.setTextColor(Color.RED);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
+        ButterKnife.bind(this, view);
+        return view;
+    }
 
-        return textView;
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        String currentUser = EMClient.getInstance().getCurrentUser();
+        settingBtnExit.setText("退出登录(" + currentUser + ")");
+
+        settingBtnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EMClient.getInstance().logout(false, new EMCallBack() {
+                    @Override
+                    public void onSuccess() {
+                        //清除数据
+                        //跳转到登录页面
+                        startActivity(new Intent(getActivity(), LoginActivity.class));
+                        //关闭当前页面
+                        getActivity().finish();
+                    }
+
+                    @Override
+                    public void onError(int i, String s) {
+
+                    }
+
+                    @Override
+                    public void onProgress(int i, String s) {
+
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }

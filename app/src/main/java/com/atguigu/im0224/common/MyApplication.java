8980e1629acc54f7a40ea66570f9e3c9d9e7a7a9
maxiaowen <332168460@qq.com>
2017-07-05 14:12:30 +0800

    * 会话界面

--- app/src/main/java/com/atguigu/im0224/common/MyApplication.java
+++ app/src/main/java/com/atguigu/im0224/common/MyApplication.java
@@ -4,8 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Handler;
 
-import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMOptions;
+import com.hyphenate.easeui.controller.EaseUI;
 
 /**
  * Created by Administrator on 2017/7/1.
@@ -33,16 +33,24 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
 
-        //初始化环信
-        EMOptions options = new EMOptions();
-     // 默认添加好友时，是不需要验证的，改成需要验证
-        options.setAcceptInvitationAlways(false);
+//        //初始化环信
+//        EMOptions options = new EMOptions();
+//     // 默认添加好友时，是不需要验证的，改成需要验证
+//        options.setAcceptInvitationAlways(false);
+//
+//        options.setAutoAcceptGroupInvitation(false);
+//       //初始化
+//        EMClient.getInstance().init(this, options);
+//      //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+//        EMClient.getInstance().setDebugMode(true);
 
+        //初始化环信sdk
+        EMOptions options = new EMOptions();//配置一些功能
+        //是否自动接受群组邀请
         options.setAutoAcceptGroupInvitation(false);
-       //初始化
-        EMClient.getInstance().init(this, options);
-      //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
-        EMClient.getInstance().setDebugMode(true);
+        //是否自动接受邀请
+        options.setAcceptInvitationAlways(false);
+        EaseUI.getInstance().init(this,options);
 
         //初始化Model
         Model.getInstance().init(this);
--- app/src/main/java/com/atguigu/im0224/controller/fragment/ConversationFragment.java
+++ app/src/main/java/com/atguigu/im0224/controller/fragment/ConversationFragment.java
@@ -1,32 +1,78 @@
 package com.atguigu.im0224.controller.fragment;
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.content.Intent;
+
+import com.atguigu.im0224.controller.activity.ChatActivity;
+import com.hyphenate.EMMessageListener;
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMConversation;
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.easeui.EaseConstant;
+import com.hyphenate.easeui.controller.EaseUI;
+import com.hyphenate.easeui.ui.EaseConversationListFragment;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/7/3.
  */
 
-public class ConversationFragment extends Fragment {
+public class ConversationFragment extends EaseConversationListFragment {
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    protected void initView() {
+        super.initView();
 
-        TextView textView = new TextView(getActivity());
+        // 监听消息会话的变化 （消息通知）
+        EMClient.getInstance().chatManager()
+                .addMessageListener(emMessageListener);
 
-        textView.setText("会话");
-        textView.setTextColor(Color.RED);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
+        //item点击事件
+        setConversationListItemClickListener(new EaseConversationListItemClickListener() {
+            @Override
+            public void onListItemClicked(EMConversation conversation) {
+                //跳转到聊天界面
+                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                intent.putExtra(EaseConstant.EXTRA_USER_ID,conversation.getUserName());
+                startActivity(intent);
+            }
+        });
+    }
 
-        return textView;
+    private EMMessageListener emMessageListener = new EMMessageListener() {
+        @Override
+        public void onMessageReceived(List<EMMessage> list) {
+            // 设置数据
+            EaseUI.getInstance().getNotifier().onNewMesg(list);
+            // 刷新列表
+            refresh();
+        }
+
+        @Override
+        public void onCmdMessageReceived(List<EMMessage> list) {
+
+        }
+
+        @Override
+        public void onMessageReadAckReceived(List<EMMessage> list) {
+
+        }
+
+        @Override
+        public void onMessageDeliveryAckReceived(List<EMMessage> list) {
+
+        }
+
+        @Override
+        public void onMessageChanged(EMMessage emMessage, Object o) {
+
+        }
+    };
+
+
+    @Override
+    protected void setUpView() {
+        super.setUpView();
+
     }
 }
